682a10b3cfcb18b08275fccbcd59303ca653588d
prietodasilvarenan-dot <prietodasilvarenan@gmail.com>
2026-03-08 22:18:01 -0300

    rota de main para pedido
    adição de rota dos botoes
    indo de uma tela para outra

--- RsHackthonIA/app/src/main/java/com/example/rshackthonia/CategoriasActivity.kt
+++ RsHackthonIA/app/src/main/java/com/example/rshackthonia/CategoriasActivity.kt
@@ -1,36 +1,20 @@
 package com.example.rshackthonia
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class CategoriasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_categorias)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val btnLocal = findViewById<Button>(R.id.btnLocal)
-        val btnViagem = findViewById<Button>(R.id.btnViagem)
-
-        btnLocal.setOnClickListener{
-            IrParaTelaPedido()
-        }
-        btnViagem.setOnClickListener{
-            IrParaTelaPedido()
-        }
-    }
-    private fun IrParaTelaPedido(){
-        val segundaTela = Intent(this, PedidoActivity::class.java)
-        startActivity(segundaTela)
     }
 }
